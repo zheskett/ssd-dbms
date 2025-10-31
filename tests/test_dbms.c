0b261449ebc6d5c9af2a7b7f649c9bfdc69ec80f
Zachary Heskett <zheskett@gmail.com>
2025-10-31 05:51:13 -0400

    Refactor test assertions for clarity and consistency; add test for tuple insertion and page filling in DBMS

--- tests/test_dbms.c
+++ tests/test_dbms.c
@@ -38,16 +38,16 @@ void tearDown() {
 }
 
 static void test_page_size() {
-  TEST_ASSERT_EQUAL_INT(sizeof(page_t), PAGE_SIZE);
+  TEST_ASSERT_EQUAL_INT(PAGE_SIZE, sizeof(page_t));
 }
 
 static void test_catalog_record_size() {
-  TEST_ASSERT_EQUAL_INT(sizeof(catalog_record_t), CATALOG_RECORD_SIZE);
+  TEST_ASSERT_EQUAL_INT(CATALOG_RECORD_SIZE, sizeof(catalog_record_t));
 }
 
 static void test_catalog_valid() {
-  TEST_ASSERT_EQUAL_UINT8(test_system_catalog.record_count, TEST_CATALOG_SIZE);
-  TEST_ASSERT_EQUAL_UINT16(test_system_catalog.tuple_size, TEST_TUPLE_SIZE);
+  TEST_ASSERT_EQUAL_UINT8(TEST_CATALOG_SIZE, test_system_catalog.record_count);
+  TEST_ASSERT_EQUAL_UINT16(TEST_TUPLE_SIZE, test_system_catalog.tuple_size);
 
   catalog_record_t* record = dbms_get_catalog_record(&test_system_catalog, 2);
   TEST_ASSERT_NOT_NULL(record);
@@ -59,10 +59,10 @@ static void test_catalog_valid() {
 
 static void test_dbms_session_init() {
   TEST_ASSERT_NOT_NULL(test_dbms_session);
-  TEST_ASSERT_EQUAL_INT(test_dbms_session->fd != -1, 1);
+  TEST_ASSERT_TRUE(test_dbms_session->fd != -1);
   TEST_ASSERT_EQUAL_STRING("test_dbms", test_dbms_session->table_name);
-  TEST_ASSERT_EQUAL_UINT8(test_dbms_session->catalog->record_count, TEST_CATALOG_SIZE);
-  TEST_ASSERT_EQUAL_UINT16(test_dbms_session->catalog->tuple_size, TEST_TUPLE_SIZE);
+  TEST_ASSERT_EQUAL_UINT8(TEST_CATALOG_SIZE, test_dbms_session->catalog->record_count);
+  TEST_ASSERT_EQUAL_UINT16(TEST_TUPLE_SIZE, test_dbms_session->catalog->tuple_size);
 }
 
 static void test_dbms_catalog_num_used() {
@@ -82,24 +82,24 @@ static void test_dbms_insert_tuple() {
   tuple_t* insert_tuple = dbms_insert_tuple(test_dbms_session, insert_attributes);
   TEST_ASSERT_NOT_NULL(insert_tuple);
   TEST_ASSERT_FALSE(insert_tuple->is_null);
-  TEST_ASSERT_EQUAL_UINT64(insert_tuple->id.slot_id, 0);
-  TEST_ASSERT_EQUAL_UINT64(insert_tuple->id.page_id, 1);
-  TEST_ASSERT_EQUAL_INT(insert_tuple->attributes[0].int_value, 1);
-  TEST_ASSERT_EQUAL_STRING(insert_tuple->attributes[1].string_value, "John Doe");
-  TEST_ASSERT_EQUAL_FLOAT(insert_tuple->attributes[2].float_value, 55000.0f);
-  TEST_ASSERT_EQUAL_STRING(insert_tuple->attributes[3].string_value, "Engineering");
+  TEST_ASSERT_EQUAL_UINT64(0, insert_tuple->id.slot_id);
+  TEST_ASSERT_EQUAL_UINT64(1, insert_tuple->id.page_id);
+  TEST_ASSERT_EQUAL_INT(1, insert_tuple->attributes[0].int_value);
+  TEST_ASSERT_EQUAL_STRING("John Doe", insert_tuple->attributes[1].string_value);
+  TEST_ASSERT_EQUAL_FLOAT(55000.0f, insert_tuple->attributes[2].float_value);
+  TEST_ASSERT_EQUAL_STRING("Engineering", insert_tuple->attributes[3].string_value);
   TEST_ASSERT_TRUE(insert_tuple->attributes[4].bool_value);
 
   buffer_page_t* fetched_buffer_page = dbms_get_buffer_page(test_dbms_session, 1);
   TEST_ASSERT_NOT_NULL(fetched_buffer_page);
-  TEST_ASSERT_EQUAL_UINT64(fetched_buffer_page->page_id, 1);
+  TEST_ASSERT_EQUAL_UINT64(1, fetched_buffer_page->page_id);
   TEST_ASSERT_TRUE(fetched_buffer_page->is_dirty);
-  TEST_ASSERT_EQUAL_UINT64(fetched_buffer_page->tuples[0].id.slot_id, 0);
-  TEST_ASSERT_EQUAL_UINT64(fetched_buffer_page->tuples[0].id.page_id, 1);
-  TEST_ASSERT_EQUAL_INT(fetched_buffer_page->tuples[0].attributes[0].int_value, 1);
-  TEST_ASSERT_EQUAL_STRING(fetched_buffer_page->tuples[0].attributes[1].string_value, "John Doe");
-  TEST_ASSERT_EQUAL_FLOAT(fetched_buffer_page->tuples[0].attributes[2].float_value, 55000.0f);
-  TEST_ASSERT_EQUAL_STRING(fetched_buffer_page->tuples[0].attributes[3].string_value, "Engineering");
+  TEST_ASSERT_EQUAL_UINT64(0, fetched_buffer_page->tuples[0].id.slot_id);
+  TEST_ASSERT_EQUAL_UINT64(1, fetched_buffer_page->tuples[0].id.page_id);
+  TEST_ASSERT_EQUAL_INT(1, fetched_buffer_page->tuples[0].attributes[0].int_value);
+  TEST_ASSERT_EQUAL_STRING("John Doe", fetched_buffer_page->tuples[0].attributes[1].string_value);
+  TEST_ASSERT_EQUAL_FLOAT(55000.0f, fetched_buffer_page->tuples[0].attributes[2].float_value);
+  TEST_ASSERT_EQUAL_STRING("Engineering", fetched_buffer_page->tuples[0].attributes[3].string_value);
   TEST_ASSERT_TRUE(fetched_buffer_page->tuples[0].attributes[4].bool_value);
 
   dbms_flush_buffer_pool(test_dbms_session);
@@ -112,15 +112,40 @@ static void test_dbms_insert_tuple() {
 
   buffer_page_t* reloaded_buffer_page = dbms_get_buffer_page(test_dbms_session, 1);
   TEST_ASSERT_NOT_NULL(reloaded_buffer_page);
-  TEST_ASSERT_EQUAL_UINT64(reloaded_buffer_page->page_id, 1);
-  TEST_ASSERT_EQUAL_INT(reloaded_buffer_page->is_dirty, false);
-  TEST_ASSERT_EQUAL_UINT64(reloaded_buffer_page->tuples[0].id.slot_id, 0);
-  TEST_ASSERT_EQUAL_UINT64(reloaded_buffer_page->tuples[0].id.page_id, 1);
-  TEST_ASSERT_EQUAL_INT(reloaded_buffer_page->tuples[0].attributes[0].int_value, 1);
-  TEST_ASSERT_EQUAL_STRING(reloaded_buffer_page->tuples[0].attributes[1].string_value, "John Doe");
-  TEST_ASSERT_EQUAL_FLOAT(reloaded_buffer_page->tuples[0].attributes[2].float_value, 55000.0f);
-  TEST_ASSERT_EQUAL_STRING(reloaded_buffer_page->tuples[0].attributes[3].string_value, "Engineering");
+  TEST_ASSERT_EQUAL_UINT64(1, reloaded_buffer_page->page_id);
+  TEST_ASSERT_EQUAL_INT(false, reloaded_buffer_page->is_dirty);
+  TEST_ASSERT_EQUAL_UINT64(0, reloaded_buffer_page->tuples[0].id.slot_id);
+  TEST_ASSERT_EQUAL_UINT64(1, reloaded_buffer_page->tuples[0].id.page_id);
+  TEST_ASSERT_EQUAL_INT(1, reloaded_buffer_page->tuples[0].attributes[0].int_value);
+  TEST_ASSERT_EQUAL_STRING("John Doe", reloaded_buffer_page->tuples[0].attributes[1].string_value);
+  TEST_ASSERT_EQUAL_FLOAT(55000.0f, reloaded_buffer_page->tuples[0].attributes[2].float_value);
+  TEST_ASSERT_EQUAL_STRING("Engineering", reloaded_buffer_page->tuples[0].attributes[3].string_value);
   TEST_ASSERT_TRUE(reloaded_buffer_page->tuples[0].attributes[4].bool_value);
+}
+
+static void test_dbms_fill_page() {
+  attribute_value_t insert_attributes[TEST_CATALOG_SIZE - 1] = {
+      {.type = ATTRIBUTE_TYPE_INT, .int_value = 1},
+      {.type = ATTRIBUTE_TYPE_STRING, .string_value = "John Doe"},
+      {.type = ATTRIBUTE_TYPE_FLOAT, .float_value = 55000.0f},
+      {.type = ATTRIBUTE_TYPE_STRING, .string_value = "Engineering"},
+      {.type = ATTRIBUTE_TYPE_BOOL, .bool_value = true}};
+
+  size_t tuples_per_page = dbms_catalog_tuples_per_page(&test_system_catalog);
+  for (size_t i = 0; i < tuples_per_page; i++) {
+    tuple_t* insert_tuple = dbms_insert_tuple(test_dbms_session, insert_attributes);
+    TEST_ASSERT_NOT_NULL(insert_tuple);
+    TEST_ASSERT_EQUAL_UINT64(1, insert_tuple->id.page_id);
+    if (i < tuples_per_page - 1) {
+      TEST_ASSERT_EQUAL_UINT64((i + 1) * (uint64_t)test_dbms_session->catalog->tuple_size,
+                               test_dbms_session->buffer_pool->buffer_pages[0].page->free_space_head);
+    } else {
+      TEST_ASSERT_EQUAL_UINT64(PAGE_SIZE, test_dbms_session->buffer_pool->buffer_pages[0].page->free_space_head);
+    }
+  }
+  TEST_ASSERT_EQUAL_UINT64(tuples_per_page, test_dbms_session->buffer_pool->buffer_pages[0].page->tuples_per_page);
+  TEST_ASSERT_EQUAL_UINT32(1, test_dbms_session->buffer_pool->page_count);
+  TEST_ASSERT_EQUAL_UINT32(1, test_dbms_session->page_count);
 }
 
 int main() {
@@ -131,6 +156,7 @@ int main() {
   RUN_TEST(test_dbms_session_init);
   RUN_TEST(test_dbms_catalog_num_used);
   RUN_TEST(test_dbms_insert_tuple);
+  RUN_TEST(test_dbms_fill_page);
 
   return UNITY_END();
 }
