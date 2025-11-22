a0eaceff63d4e32685f157da9abe6e7351aa51f1
Zachary Heskett <zheskett@gmail.com>
2025-11-21 20:20:27 -0500

    reduced size of insertion test

--- generate_test.sh
+++ generate_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 open_string="open test1.dat\nopen test2.dat\nopen test3.dat\nopen test4.dat\nopen test5.dat\nopen test6.dat\n"
-time_string_1="time split false test1 fill 1000000 0;test2 fill 1000000 0;test3 fill 1000000 0\n"
-time_string_2="time split true test4 fill 1000000 0;test5 fill 1000000 0;test6 fill 1000000 0\n"
+time_string_1="time split false test1 fill 100000 0;test2 fill 100000 0;test3 fill 100000 0\n"
+time_string_2="time split true test4 fill 100000 0;test5 fill 100000 0;test6 fill 100000 0\n"
 exit_string="exit\n"
 rm -f test.dat test1.dat test2.dat test3.dat test4.dat test5.dat test6.dat
 echo -e "create test.dat\nfield_1\n1\nfield_2\n2\nfield_3\n3\n32\nfield_4\n4\nfinish\nexit\n" | ./build/ssd-dbms-cli
